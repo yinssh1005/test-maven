3cf794e52433940e7cd8416e9192a71eaec566cf
Ethan Yin <yinssh1005@gmail.com>
2021-08-24 16:56:38 +0800

    Initial commit

---
+++ src/main/java/com/testmaven/TestGBK.java
@@ -0,0 +1,7 @@
+package com.testmaven;
+
+public class TestGBK {
+    /*
+        这里是用GBK编码写的内容，中午
+     */
+}
---
+++ src/test/java/com/testmaven/TestLombokTest.java
@@ -0,0 +1,21 @@
+package com.testmaven;
+
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestLombokTest {
+
+    @org.junit.jupiter.api.Test
+    void returnAStr() {
+        Assertions.assertFalse(true);
+    }
+
+    @org.junit.jupiter.api.Test
+    void getStr1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setStr1() {
+    }
+}
