ffa469cf3374f6fa2c5d1d84b61bccc7ba3dd6e2
Ethan Yin <yinssh1005@gmail.com>
2021-09-03 14:09:05 +0800

    test in tc

--- src/main/java/com/testmaven/TestGBK.java
+++ src/main/java/com/testmaven/TestGBK.java
@@ -1,11 +1,8 @@
 package com.testmaven;
 
 public class TestGBK {
-    /*
-        这里是用GBK编码写的内容，中午
-     */
     public static void main(String[] args) {
-        OnePiece onePiece = new OnePiece(10);
-        System.out.println(onePiece.toString());
+        String abc = "这是一个字符串\\测试\\测试";
+        System.out.println(abc);
     }
 }
