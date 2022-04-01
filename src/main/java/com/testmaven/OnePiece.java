584c42381db43522c1da63f12fe095765b1c8879
ethan <yinssh1005@gmail.com>
2022-04-01 11:47:09 +0800

    新的变更列表

--- src/main/java/com/testmaven/OnePiece.java
+++ src/main/java/com/testmaven/OnePiece.java
@@ -3,24 +3,6 @@ package com.testmaven;
 
 public class OnePiece {
 
-    private int testInt;
 
-    public int getTestInt() {
-        return testInt;
-    }
 
-    public void setTestInt(int testInt) {
-        this.testInt = testInt;
-    }
-
-    public OnePiece(int testInt) {
-        this.testInt = testInt;
-    }
-
-    @Override
-    public String toString() {
-        return "OnePiece{" +
-                "testInt=" + testInt +
-                '}';
-    }
 }
